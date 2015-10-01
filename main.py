5a414633729f08ecf0d924c1c328c8f94812bafb
Clément Robert <clement.robert@protonmail.com>
2015-10-01 21:32:48 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+#-*-coding:utf-8-*-
+
+from classes import *
+
+import numpy as np
+import pylab as pl
+
+#-------------------------
+# script
+#-------------------------
+
+bench = CRSet(int(1e5))
+while bench.epoch < MAXEPOCH and not bench.isDead :
+    bench.walk0()#not well understood
+
+
+#-------------------------
+# plotting
+#-------------------------
+
+#...
