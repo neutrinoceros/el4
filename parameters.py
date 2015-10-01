a6f51da5c178583033399bd78ec71a74e59e1d0e
Clément Robert <clement.robert@protonmail.com>
2015-10-01 21:32:04 +0200

    Create parameters.py

---
+++ parameters.py
@@ -0,0 +1,22 @@
+#-*-coding:utf-8-*-
+
+#-------------------------
+# parameters
+#-------------------------
+LIGHTC=3.066e2#kpc/Myr
+RCR=7#kpc
+ALPHA=3#adim
+SIGMA0=5e7#Msol/kpc^2
+RGAS=10#kpc
+KAPPA=5e-12#kpc^2/Msol
+GAMMA=1#adim
+EMIN=1e2#GeV
+EMAX=1e7#GeV
+H0=4#kpc
+LAMBDA0=1e-3#kpc à 1 GeV (???)
+BETA=0.5#adim
+
+
+##
+TIMESTEP=1.#???
+MAXEPOCH=1e8 #???
