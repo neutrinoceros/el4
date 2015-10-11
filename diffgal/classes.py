added correct normalization of velocity

diffgal/classes.py
@@ -28,8 +28,8 @@ class CosmicRay :
         self.absorbed = False
         self.escaped = False
 
-        #if rand() < absorptionProb(self.r)/2 :#une "demie traversée" à la naissance
-        #    self.absorbed = True
+        if rand() < self.absorptionProb()/2 :#une "demie traversée" à la naissance
+            self.absorbed = True
 
 
     def udtheta(self) :
@@ -38,8 +38,11 @@ class CosmicRay :
 
     def getnextpos(self,ts=TIMESTEP):
         #NB : acutal radial and vertical velocities need to be adjusted to take azimutal vel into account
-        nextr = np.abs(self.r + LIGHTC * np.sin(self.theta) * ts)#radial projection of the velocity aplied
-        nexth = self.h + LIGHTC * np.cos(self.theta) * ts
+        azimutal_v = LIGHTC * np.sin(self.phi) 
+        radial_v   = LIGHTC * np.cos(self.phi) * np.sin(self.theta)
+        vertical_v = LIGHTC * np.cos(self.phi) * np.cos(self.theta)
+        nextr = np.abs(self.r + radial_v * ts)
+        nexth = self.h + vertical_v * ts
         return nextr,nexth
 
 
@@ -124,9 +127,9 @@ class CRSet :
             else :
                 r_alive.append(ray.r) 
                 h_alive.append(ray.h)
-        pl.scatter(r_alive,h_alive,color="blue",alpha=0.8,s=.5)
-        pl.scatter(r_abs,h_abs,color="red",alpha=0.8,s=.5)
-        pl.scatter(r_esc,h_esc,color="green",alpha=0.8,s=.5)
+        pl.scatter(r_alive,h_alive,color="blue",alpha=ALPHA,s=SIZE)
+        pl.scatter(r_abs,h_abs,color="red",alpha=ALPHA,s=SIZE)
+        pl.scatter(r_esc,h_esc,color="green",alpha=ALPHA,s=SIZE)
 
         #rmin,rmax=ax.get_xlim()
         rrr=np.linspace(0,1e3,100)